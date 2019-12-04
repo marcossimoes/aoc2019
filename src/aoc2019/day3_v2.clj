a45b6b789c0e63feb61102599e282e6f97888aae
Marcos Simões <marcos.simoes@nubank.com.br>
2019-12-04 09:05:05 -0300

    add solution to day3 v2

--- src/aoc2019/day3_v2.clj
+++ src/aoc2019/day3_v2.clj
@@ -2,10 +2,10 @@
 
 (defn str->int [s] (Integer. s))
 
-(defn right [pt dist] (update pt 0 + dist))
-(defn left [pt dist] (update pt 0 - dist))
-(defn up [pt dist] (update pt 1 + dist))
-(defn down [pt dist] (update pt 1 - dist))
+(defn right [pt dist] (-> pt (update 0 + dist) (update 2 conj dist)))
+(defn left [pt dist] (-> pt (update 0 - dist) (update 2 conj dist)))
+(defn up [pt dist] (-> pt (update 1 + dist) (update 2 conj dist)))
+(defn down [pt dist] (-> pt (update 1 - dist) (update 2 conj dist)))
 
 (defn char-seq->coord
   "converts a sequence of characters with c d d d format (any number of digits allowed)
@@ -41,13 +41,13 @@
                             second
                             (pt+vec->line v)
                             (as-> new-line (conj lines new-line))))
-          [(pt+vec->line [0 0] first-vec)]
+          [(pt+vec->line [0 0 []] first-vec)]
           rem-wire))
 
 (def wire1 (draw-wire (first (wires-from-file-path "resources/day-3-input-test-1"))))
 (def wire2 (draw-wire (second (wires-from-file-path "resources/day-3-input-test-1"))))
 
-(defn intercept?
+(defn intersect?
   [[[x11 y11] [x12 y12]]
    [[x21 y21] [x22 y22]]]
   (and (<= (min x11 x12) (max x21 x22))
@@ -60,7 +60,7 @@
 (defn const-coord
   "assumes coordinates are from either vertical or
   horizontal lines, and that lines were already
-  checked for interception. In most cases, either
+  checked for intersection. In most cases, either
   c11 = c12 or c21 = c22. In the exceptional case that
   the lines are parallel and not on this coord,
   gets the value with smallest absolute value
@@ -72,23 +72,37 @@
     (< (abs c21) (abs c22)) c21
     (> (abs c21) (abs c22)) c22))
 
-(defn intercept-cord
+(defn intersect-cord
   [coord line1 line2]
   (->> line2
        (into line1)
        (map coord)
        (apply const-coord)))
 
-(defn intercept [line1 line2]
-  (when (intercept? line1 line2)
-    [(intercept-cord first line1 line2)
-     (intercept-cord second line1 line2)]))
+(defn dist-line-start-to-to-pt
+  [[[x-start y-start]] [x-pt y-pt]]
+  (+ (abs (- x-start
+             x-pt))
+     (abs (- y-start
+             y-pt))))
+
+(defn intersect [line1 line2]
+  (when (intersect? line1 line2)
+    (let [x (intersect-cord first line1 line2)
+          y (intersect-cord second line1 line2)
+          acc-steps-line1 (get-in line1 [1 2])
+          acc-steps-line2 (get-in line2 [1 2])
+          step1-to-intsct (dist-line-start-to-to-pt line1 [x y])
+          step2-to-intsct (dist-line-start-to-to-pt line2 [x y])
+          acc-steps-line1-at-intsct (assoc acc-steps-line1 (dec (count acc-steps-line1)) step1-to-intsct)
+          acc-steps-line2-at-intsct (assoc acc-steps-line2 (dec (count acc-steps-line2)) step2-to-intsct)]
+      [x y [acc-steps-line1-at-intsct acc-steps-line2-at-intsct]])))
 
 (defn check-all-wire-line-pairs-for-intersections
   [wire1 wire2]
   (for [line1 wire1
         line2 wire2]
-    (intercept line1 line2)))
+    (intersect line1 line2)))
 
 (defn wire-intersections
   [wire1 wire2]
@@ -98,11 +112,9 @@
        ;remove checks that returned nil
        (filter some?)))
 
-(wire-intersections wire1 wire2)
-
 (defn pt-manhattan-dist
   [pt]
-  (->> pt
+  (->> (pop pt)
        ;; get absolute value
        (map #(max % (- %)))
        (apply +)))
@@ -114,4 +126,17 @@
         first-pt-dist (pt-manhattan-dist first-pt)]
     (transduce (map pt-manhattan-dist) min first-pt-dist rem-pts)))
 
+(defn combined-steps [[_ _ [steps1 steps2]]]
+  (apply + (into steps1 steps2)))
+
+(defn min-combined-steps-to-intersection
+  [input-file-path]
+  (let [[wire1 wire2] (map draw-wire (wires-from-file-path input-file-path))
+        [first-pt & rem-pts] (wire-intersections wire1 wire2)
+        _ (println "intersect:" (into [first-pt] rem-pts) "\n")
+        first-pt-dist (combined-steps first-pt)]
+    (transduce (map combined-steps) min first-pt-dist rem-pts)))
+
 (min-of-manhathan-dist-over-intersections "resources/day-3-input")
+
+(min-combined-steps-to-intersection "resources/day-3-input")
